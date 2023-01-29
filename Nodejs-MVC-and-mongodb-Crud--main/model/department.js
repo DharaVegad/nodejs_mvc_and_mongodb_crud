b64fdd2deed58b73c6e431ba484b3cf8ea2fce13
Dhara Vegad <75181096+DharaVegad@users.noreply.github.com>
2023-01-29 16:47:23 -0500

    Delete Nodejs-MVC-and-mongodb-Crud--main directory

--- Nodejs-MVC-and-mongodb-Crud--main/model/department.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-ObjectId = Schema.ObjectId;
-var mydepartment = new Schema({
-    departmentid:{ type:Number,required:true},
-    departmentname: {type:String,required:true}
-});
-module.exports=mongoose.model('departments',mydepartment);
